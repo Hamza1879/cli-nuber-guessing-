ea820aad3203f0962c24ebbd3b4a82aa0ed6b1da
Hamza1879 <M.hamza1879@gmail.com>
2024-04-15 03:30:15 -0700

    cli based number guessing game

---
+++ inddex.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*6 + 1);
+
+const answers=await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number between 1-6: ",  
+    },
+]);
+
+if(answers.userguessednumber === randomNumber){
+    console.log("congragulations! you guess right number.")
+}else{
+    console.log("you guessed wrong number.")
+} 
